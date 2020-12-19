d33e71e0d7ba080cdb2c799ed5719acb52e6a4f9
skdrums <sora_kannaduki1009@gmail.com>
2020-12-19 10:07:23 +0900

    feat: add getNetworkActivities method

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
 window.alert('アプリ開いたね！');
+js =
+    "var performance = window.performance || window.mozPerformance" +
+    " || window.msPerformance || window.webkitPerformance || {};" +
+    " return performance.getEntries() || {};";
+netData = executeJavaScript(js).toString();
+logger.info("Network traffic: {}", netData);
+console.log("Network traffic: {}", netData);
+window.alert("Network traffic: {}", netData);
