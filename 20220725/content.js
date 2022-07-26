45271b2f780c76c128513d52dc86d3f4513f27b5
skdrums <sora_kannaduki1009@gmail.com>
2022-07-26 23:15:37 +0900

    add new chrome extension

---
+++ 20220725/content.js
@@ -0,0 +1,18 @@
+// $(function() {
+//   console.log("コンテントスクリプトだよ");
+//   var st = document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-1dbjc4n.r-1loqt21.r-136ojw6 > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1777fci > div > h2 > span");
+//   console.log("skdrums1");
+//   console.log("skdrums2", st);
+// });
+$(function() {
+  console.log("コンテントスクリプトだよ");
+  var st = document.querySelector.querySelector("#caption-window-1 > span > span > span");
+  console.log("skdrums1");
+  console.log("skdrums2", st);
+});
+//
+// $(function() {
+//   console.log("コンテントスクリプトだよ");
+//   $('body').css('background-color','orange');
+// });
+// document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-1dbjc4n.r-1loqt21.r-136ojw6 > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1777fci > div > h2 > span")
